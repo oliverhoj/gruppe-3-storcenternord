9bf9bbe51959bbe95649d8b6085b6e819f70667f
oliverhoj <eaa25ohh@students.eaaa.dk>
2025-11-11 12:26:42 +0100

    Add files via upload

---
+++ exd-storcenternord-gruppe-3/exd-storcenternord/exd-akvarie/js/animate-objects-on-road.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// funktion der afspiller lyd når boble klikkes og tilføjer pop animation
+function handleBubbleClick(event) {
+  const bubbleSound = new Audio("../sound/bubble-pop.mp3"); //ændrer lydfil til boblelyd
+  bubbleSound.play();
+}
+
+// venter med at hente js siden tilden hele html er loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Add click handlers to all bubbles
+  document.querySelectorAll(".bubble").forEach((bubble) => {
+    bubble.addEventListener("click", handleBubbleClick);
+  });
+});
+
+
+// lav den nu med skattekisten, når man hover over den så skal der afspilles en lyd af skattekisten der åbnes, med skattekiste-sounden
+function handleSkattekisteHover(event) {
+  const skattekisteSound = new Audio("../sound/skattekiste-sound.mp3");
+  skattekisteSound.play();
+}
+
+// venter med at hente js siden tilden hele html er loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Add hover handler to skattekiste
+  const skattekiste = document.getElementById("skattekiste");
+  skattekiste.addEventListener("mouseover", handleSkattekisteHover);
+});
